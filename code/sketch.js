623ab69e8d001ab8f40f7f53e64d92ed431cb29b
F4Tornado <h.rovnyak@gmail.com>
2020-05-24 16:17:52 -0400

    Add comments

--- code/sketch.js
+++ code/sketch.js
@@ -1,9 +1,11 @@
+// Set up the canvas
 const c = document.getElementById("c");
 const draw = c.getContext("2d");
 
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 
+// Get the web worker to generate the heightmap
 let terrainWidth = c.width * 2;
 let terrainHeight = c.height * 2
 
@@ -24,30 +26,34 @@ terrainGenerater.onmessage = (msg) => {
 function drawLoop() {
   setTimeout(drawLoop, 1000 / 60);
 
+  // Draw the background
   draw.fillStyle = "#000";
   draw.fillRect(0, 0, c.width, c.height);
 
   if (heightmap) {
+    // Create the image data
     let imageData = draw.getImageData(0, 0, c.width, c.height);
     let pixels = imageData.data;
+    // Draw each pixel
     for (let i = 0; i < pixels.length; i += 4) {
+      // get the height to draw on screen using the heightmap
       let terrainpos = [(i / 4) % c.width, Math.floor((i / 4) / c.height)];
       let terraini = terrainpos[1] * terrainHeight + terrainpos[0];
       let brightness = heightmap[terraini];
 
-      if (brightness < 50) {
+      if (brightness < 50) { // Oceans
         pixels[i] = 0;
         pixels[i + 1] = map(brightness, 0, 50, 64, 128);
         pixels[i + 2] = map(brightness, 0, 50, 128, 255);
-      } else if (brightness < 60) {
+      } else if (brightness < 60) { // Beaches
         pixels[i] = 255;
         pixels[i + 1] = 255;
         pixels[i + 2] = 128;
-      } else if (brightness < 140) {
+      } else if (brightness < 140) { // Plains
         pixels[i] = map(brightness, 60, 140, 64, 128);
         pixels[i + 1] = map(brightness, 60, 140, 128, 255);
         pixels[i + 2] = map(brightness, 60, 140, 64, 128);
-      } else {
+      } else { // Mountain tops
         pixels[i] = brightness;
         pixels[i + 1] = brightness;
         pixels[i + 2] = brightness;
--- code/terrain.js
+++ code/terrain.js
@@ -9,11 +9,14 @@ onmessage = (msg) => {
   width = msg.data[0];
   height = msg.data[1];
 
+  // Start the generation when given the width & height
   generateTerrain();
 }
 
 function generateTerrain() {
   console.log("starting");
+
+  // Use perlin noise to generate each pixel
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
       heightmap.push(Math.abs(noise.perlin2(j / 1000, i / 1000)) * 256);
